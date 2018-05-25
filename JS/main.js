bbdda000e240dab564f4459ba91cff99304e5093
mac <mac@macs-MacBook-Pro.local>
2018-05-25 12:03:33 -0600

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+var cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+if (cardsInPlay.length === 2); {
+	else if ()
+}
