d5ff61401e6607ca8e5e0b47406ae629169397ea
mac <mac@macs-MacBook-Pro.local>
2018-05-29 14:54:01 -0600

    created game logic2

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,27 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardsInPlay = [];
+var cardsInPlay = []; 
 
-// Create cardOne to show as the first card the user flipped
-var cardOne = cards[0];
-// Add the card to the array of cards that are in use
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-// Create cardTwo to show the second card the user flipped
-var cardTwo = cards[2];
-// Add the card to the array of cards that are in use
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-
-// Checks to see if cards have been used
-if (cardsInPlay.length === 2) {
-
-  // Verify match and user response
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	 {
+  } if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
-
-  }
 }
+}
+
+var cardFlip = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]); 
+
+	if (cardsInPlay.length === 2); {
+
+	checkForMatch();
+ 	}
+}
+
+
+cardFlip(0);
+cardFlip(2);
